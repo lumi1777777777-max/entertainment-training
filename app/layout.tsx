8d0ec609c7925f6768c832d3c1ac0107d0de9913
lumi1777777777-max <lumi1777777777@gmail.com>
2026-05-31 10:32:05 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Entertainment Division Training",
+  description: "MainStreet RP training website",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="et">
+      <body>{children}</body>
+    </html>
+  );
+}
